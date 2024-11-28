80f9804529fa270f79b6ccd9ca8ebfc9f2e3d80b
Sander <sander@concept7.nl>
2024-11-28 16:14:53 +0100

    logo

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <img class="w-auto h-40 mx-auto" src="{{ Vite::asset('resources/images/logo.png') }}" alt="logo">
+    <img class="w-auto h-40 mx-auto" src="{{ asset('images/logo.png') }}" alt="logo">
 </div>
