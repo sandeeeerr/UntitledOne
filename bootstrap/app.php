b7defc482f9dfbf49bc402042676f86fca32fa99
Sander <sander@concept7.nl>
2024-12-03 11:58:15 +0100

    fillament

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,16 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+$envFile = '.env';
+
+if (!empty($_SERVER['APP_ENV'])) {
+    $envFile = '.env.' . $_SERVER['APP_ENV'];
+} elseif (!empty(getenv('APP_ENV'))) {
+    $envFile = '.env.' . getenv('APP_ENV');
+}
+
+// Controleer of het bestand bestaat en laad het
+$app->loadEnvironmentFrom($envFile);
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,3 +26,5 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+
